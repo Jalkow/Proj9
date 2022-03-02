1aedcf2b3b79bc90cb4499f99f5c3cfbf591e335
Dorian Gehant <dgehant@gaming.tech>
2022-03-02 12:25:14 +0100

    recuperation articles dans la page des produits

--- react/src/components/Produits.js
+++ react/src/components/Produits.js
@@ -2,11 +2,32 @@ import React, {Component} from 'react';
 
 
 class Produits extends Component{
+    constructor(props) {
+      super(props)
+    }
     render() {
+        console.log(this.props)
+        if(this.props.loading) {
+            return(
+                <h2>Loading ...</h2>
+            )
+        }
         return(
-            <h2>Test</h2>
+            <>
+            <h1>Hello</h1>
+            <ul>
+                {
+                    this.props.articles && this.props.articles.map((article,i) =>{
+                        return <li key={i}>{article.attributes.name}</li>
+                    })
+                }
+            </ul>
+            </>
         );
+        
+        
     }
 }
 
 export default Produits;
+
--- react/src/index.js
+++ react/src/index.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { Component } from 'react';
+import ReactDOM, { render } from 'react-dom';
 import './index.css';
 import App from './App';
 import Produits from './components/Produits';
@@ -9,15 +9,35 @@ import { BrowserRouter,
 } from 'react-router-dom';
 
 
-const Root = () => {
-  return(
-  <BrowserRouter>
-    <Routes>
-      <Route exact path="/" element={<App />} />
-      <Route exact path="/produits" element={<Produits />} />
-    </Routes>
-  </BrowserRouter>
-  );
+class Root extends Component{
+
+  constructor(props) {
+    super(props)
+    this.state={
+       articles:[],
+       loading:true
+    }
+  }
+
+  async componentDidMount () {
+    const response = await fetch('http://localhost:1337/api/Articles', {method: 'GET', headers: {'Accept': 'application/json', 'Content-Type':'application/json'}})
+    const articles = await response.json()
+    this.setState({
+      articles:articles,
+      loading:false
+    });
+  }
+
+  render(){
+    return(
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<App />} />
+          <Route exact path="/produits" element={<Produits articles={this.state.articles.data} loading={this.state.loading} />} />
+        </Routes>
+      </BrowserRouter>
+    );
+  }
 }
 
 ReactDOM.render(<Root />, document.getElementById('root'));
